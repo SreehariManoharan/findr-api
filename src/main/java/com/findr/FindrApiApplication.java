e57b7b24717cef91eb318dde1889e80288285cbb
SreehariManoharan <sreeharimanoharan42@gmail.com>
2024-04-28 07:04:51 +0200

    initial commit

---
+++ src/main/java/com/findr/FindrApiApplication.java
@@ -0,0 +1,13 @@
+package com.findr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindrApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findr/FindrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindrApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
